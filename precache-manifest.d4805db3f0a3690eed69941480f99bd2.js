95afb911b479e32814c31ce056b33df960716d24
ThalitaC <thalitaceci@gmail.com>
2021-06-03 13:08:10 -0300

    Updates

---
+++ precache-manifest.d4805db3f0a3690eed69941480f99bd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7995c364d78b50188387ff2b82e8bd89",
+    "url": "/trybe-project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "57a3e7f994188f7f4ae2",
+    "url": "/trybe-project-movie-cards-library/static/css/main.ddb28dd1.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/trybe-project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "57a3e7f994188f7f4ae2",
+    "url": "/trybe-project-movie-cards-library/static/js/main.8d2e1c62.chunk.js"
+  },
+  {
+    "revision": "da432151a4c4f2487304",
+    "url": "/trybe-project-movie-cards-library/static/js/runtime-main.5891c939.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/trybe-project-movie-cards-library/precache-manifest.d4805db3f0a3690eed69941480f99bd2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybe-project-movie-cards-library/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
